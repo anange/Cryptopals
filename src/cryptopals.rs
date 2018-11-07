f0670bad872da75bddadb9c6b6831b9b9e859cb8
anange <a.angelakis@outlook.com>
2018-11-07 02:12:52 +0200

    WIP: Challenge6

--- src/cryptopals.rs
+++ src/cryptopals.rs
@@ -1,33 +1,120 @@
+const BASE64: [char; 64] = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+    'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
+];
+
+pub fn decrypt_vigenere(text: &str) -> String {
+    let bytes = String::from(text).into_bytes();
+    let keysize = get_possible_keysize(&bytes);
+    //println!("keysize={}", keysize);
+    let mut slices = Vec::new();
+    for c in bytes.chunks(keysize) {
+        slices.push(c);
+    }
+    let mut same_byte_slices = Vec::new();
+    for ind in 0..keysize {
+        let mut cur_same_byte_slice = Vec::new();
+        for s in &slices {
+            if s.len() <= ind {
+                break
+            }
+            cur_same_byte_slice.push(s[ind]);
+        }
+        same_byte_slices.push(cur_same_byte_slice);
+    }
+    let mut key: Vec<u8> = Vec::new();
+    for s in &same_byte_slices {
+        let (_, _, key_byte) = decode_byte_xor_cipher(&bytes_to_hex(s.clone()));
+        key.push(key_byte);
+    }
+    let bytes = hex_to_bytes(encrypt_repeating_xor(&bytes, &key).as_str());
+    use std::str;
+    let decoded = str::from_utf8(&bytes);
+    if decoded.is_err() {
+        return String::new()
+    }
+    decoded.unwrap().to_string()
+
+}
+
+pub fn get_possible_keysize(bytes: &Vec<u8>) -> usize {
+    let mut min_dist: f64 = 8.0;
+    let mut best_keysize = 2;
+    let blocks = 3;
+    for keysize in 2..41 {
+        let mut cur_dist = 0;
+        for i in 0..blocks {
+            let first: Vec<u8> = bytes[2*i*keysize..(2*i+1) * keysize].to_vec();
+            let second: Vec<u8> = bytes[(2*i+1)*keysize..(2*i+2) * keysize].to_vec();
+            cur_dist += hamming_distance(&first, &second);
+        }
+        if ((cur_dist as f64) / (blocks * keysize) as f64) < min_dist {
+            min_dist = (cur_dist as f64) / (blocks  * keysize) as f64;
+            best_keysize = keysize;
+        }
+    }
+    best_keysize
+}
+
+
 pub fn hex_to_b64(hex: &str) -> String {
     if hex.len() % 2 != 0 {
         panic!("Hex string should have even digits");
     }
     let bytes = hex_to_bytes(hex);
-    let mut b64_conv = B64Converter::new(bytes);
+    let mut b64_conv = ToB64Converter::new(bytes);
     b64_conv.convert()
 }
 
-struct B64Converter {
+pub fn b64_to_string(b64_enc: &str) -> String {
+    use std::str;
+    let mut bytes: Vec<u8> = Vec::new();
+    let mut cur_byte: u8 = 0u8;
+    let mut base64_index = 0;
+    for ch in b64_enc.chars() {
+        if ch == '=' {
+            break
+        } else if ch == '\n' {
+            continue
+        }
+        let b64_value: u8 = BASE64.iter()
+            .position(|&r| r == ch).expect(&ch.to_string()) as u8;
+        match base64_index % 4 {
+            0 => cur_byte = b64_value << 2,
+            1 => {
+                cur_byte += b64_value >> 4;
+                bytes.push(cur_byte);
+                cur_byte = b64_value << 4;
+            },
+            2 => {
+                cur_byte += b64_value >> 2;
+                bytes.push(cur_byte);
+                cur_byte = b64_value << 6;
+            },
+            _ => bytes.push(cur_byte + b64_value)
+        }
+        base64_index += 1;
+    }
+    str::from_utf8(&bytes).unwrap().to_string()
+}
+
+struct ToB64Converter {
     bytes: Vec<u8>,
     bit_index: usize,
     byte_index: usize,
 }
 
-impl B64Converter {
-    pub fn new(bytes: Vec<u8>) -> B64Converter {
+impl ToB64Converter {
+    pub fn new(bytes: Vec<u8>) -> ToB64Converter {
         let bit_index = 0;
         let byte_index = 0;
-        B64Converter { bytes, bit_index, byte_index }
+        ToB64Converter { bytes, bit_index, byte_index }
     }
-    
+
     pub fn next(&mut self) -> Option<char> {
-        let base64 = [
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
-        ];
         if self.byte_index == self.bytes.len() {
             return None
         }
@@ -38,7 +125,7 @@ impl B64Converter {
             cur += self.bytes[self.byte_index + 1] >> (10 - self.bit_index);
         }
         self.increase_bit_index();
-        Some(base64[cur as usize])
+        Some(BASE64[cur as usize])
     }
 
     fn increase_bit_index(&mut self) {
@@ -129,12 +216,14 @@ pub fn hex_to_char(c: u8) -> char {
     }
 }
 
-pub fn decode_byte_xor_cipher(hex: &str) -> (String, usize) {
+pub fn decode_byte_xor_cipher(hex: &str) -> (String, usize, u8) {
     use std::str;
+    //let common_letters = ['e'];
     let common_letters = ['e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r'];
     let bytes = hex_to_bytes(hex);
     let mut cur_score: usize;
     let mut max_score = 0;
+    let mut decrypt_key: u8 = 0u8;
     let mut best_match: Vec<u8> = Vec::new();
     for key in 0u8..255u8 {
         cur_score = 0;
@@ -142,7 +231,15 @@ pub fn decode_byte_xor_cipher(hex: &str) -> (String, usize) {
         for c in &xored {
             for letter in common_letters.iter() {
                 if *letter == *c as char {
-                    cur_score += 1;
+                    if *letter == 'e' {
+                        cur_score += 4;
+                    } else if *letter == 't' {
+                        cur_score += 3;
+                    } else if *letter == 'r' || *letter == 'h' || *letter == 's' {
+                        cur_score += 1;
+                    } else {
+                        cur_score += 2;
+                    }
                     break;
                 }
             }
@@ -150,13 +247,14 @@ pub fn decode_byte_xor_cipher(hex: &str) -> (String, usize) {
         if max_score < cur_score {
             max_score = cur_score;
             best_match = xored;
+            decrypt_key = key;
         }
     }
     let decoded = str::from_utf8(&best_match);
     if decoded.is_err() {
-        return (String::new(), 0)
+        return (String::new(), 0, 0)
     }
-    (str::from_utf8(&best_match).unwrap().to_string(), max_score)
+    (str::from_utf8(&best_match).unwrap().to_string(), max_score, decrypt_key)
 }
 
 pub fn xor_with_byte(bytes: &Vec<u8>, key: u8) -> Vec<u8> {
@@ -167,14 +265,35 @@ pub fn xor_with_byte(bytes: &Vec<u8>, key: u8) -> Vec<u8> {
     xored_bytes
 }
 
-pub fn encrypt_repeating_xor(text: &str, key: &str) -> String {
-    let text_bytes = String::from(text).into_bytes();
-    let key_bytes = String::from(key).into_bytes();
+pub fn encrypt_repeating_xor(text_bytes: &[u8], key_bytes: &[u8]) -> String {
     let mut encrypted_bytes: Vec<u8> = Vec::new();
     let mut key_index = 0;
     for byte in text_bytes {
         encrypted_bytes.push(byte ^ key_bytes[key_index]);
-        key_index = (key_index + 1) % 3;
+        key_index = (key_index + 1) % key_bytes.len();
     }
     bytes_to_hex(encrypted_bytes)
+}
+
+pub fn hamming_distance(text1_bytes: &[u8], text2_bytes: &[u8]) -> usize {
+    if text1_bytes.len() != text2_bytes.len() {
+        panic!("Strings have to be of equal length");
+    }
+    let mut dist = 0;
+    for i in 0..text1_bytes.len() {
+        dist += byte_hamming_distance(text1_bytes[i], text2_bytes[i]);
+    }
+    dist
+}
+
+pub fn byte_hamming_distance(byte1: u8, byte2: u8) -> usize {
+    let mut xored = byte1 ^ byte2;
+    let mut dist = 0;
+    while xored > 0 {
+        if xored % 2 == 1 {
+            dist += 1;
+        }
+        xored >>= 1;
+    }
+    dist
 }
--- src/lib.rs
+++ src/lib.rs
@@ -39,7 +39,7 @@ mod tests {
         let mut max_score = 0;
         let mut correct = String::new();
         for line in &ciphers {
-            let (decoded, score) = decode_byte_xor_cipher(line);
+            let (decoded, score, _) = decode_byte_xor_cipher(line);
             if score > max_score {
                 max_score = score;
                 correct = decoded;
@@ -51,8 +51,59 @@ mod tests {
     #[test]
     fn challenge5() {
         use cryptopals::encrypt_repeating_xor;
-        let a = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+        let a = String::from("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal");
+        let key = String::from("ICE");
         let encrypted = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
-        assert_eq!(encrypted, encrypt_repeating_xor(a, "ICE"));
+        assert_eq!(encrypted, encrypt_repeating_xor(&a.into_bytes(), &key.into_bytes()));
+    }
+
+    #[test]
+    fn test_hamming_distance() {
+        use cryptopals::hamming_distance;
+        let a = String::from("this is a test").into_bytes();
+        let b = String::from("wokka wokka!!!").into_bytes();
+        assert_eq!(37, hamming_distance(&a, &b));
+    }
+
+    #[test]
+    fn test_base64_decode() {
+        use cryptopals::b64_to_string;
+        let enc1 = "YW55IGNhcm5hbCBwbGVhcw==";
+        let dec1 = "any carnal pleas";
+        assert_eq!(b64_to_string(enc1), dec1);
+        let enc2 = "YW55IGNhcm5hbCBwbGVhc3U=";
+        let dec2 = "any carnal pleasu";
+        assert_eq!(b64_to_string(enc2), dec2);
+        let enc3 = "YW55IGNhcm5hbCBwbGVhc3Vy";
+        let dec3 = "any carnal pleasur";
+        assert_eq!(b64_to_string(enc3), dec3);
+    }
+
+    #[test]
+    fn challenge6_test() {
+        use std::fs::File;
+        use std::io::Read;
+        let mut file = File::open("data/6-test.txt").unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).expect("Can't read file");
+
+        use cryptopals::b64_to_string;
+        use cryptopals::decrypt_vigenere;
+        let encrypted = b64_to_string(&contents);
+        assert_eq!("lalala", decrypt_vigenere(&encrypted));
+    }
+
+    #[test]
+    fn challenge6() {
+        use std::fs::File;
+        use std::io::Read;
+        let mut file = File::open("data/6.txt").unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).expect("Can't read file");
+
+        use cryptopals::b64_to_string;
+        use cryptopals::decrypt_vigenere;
+        let encrypted = b64_to_string(&contents);
+        assert_eq!("lalala", decrypt_vigenere(&encrypted));
     }
 }
